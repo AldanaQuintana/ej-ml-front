7c781f22bf5d3fe73a0f1660a43f25869e69fadd
Aldana Quintana <aldanaqm@gmail.com>
2017-02-19 15:46:27 -0300

    Initialize search box with value on main.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,19 @@ import SearchPageApp from './components/SearchPageApp.jsx';
 import ResultsPageApp from './components/ResultsPageApp.jsx';
 import _ from 'lodash';
 
+
+const searchRegex = /(\?|&)search=([^&]*)/;
+
+const searchQuery = window.location.search;
+let searchParam = '';
+if ( searchRegex.test(searchQuery) ){
+  searchParam = searchQuery.match(searchRegex,'')[2];
+  searchParam = searchParam.split("+").join(" ");
+}
+
 const apps = {
   "search-page": <SearchPageApp />,
-  "results-page": <ResultsPageApp />
+  "results-page": <ResultsPageApp value={searchParam} />
 };
 
 _.each(apps, function(app, nodeId){
