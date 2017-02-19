b07ea73f7be01d21046139526ae916b5798cc574
Aldana Quintana <aldanaqm@gmail.com>
2017-02-19 13:31:49 -0300

    Add components to app

--- src/components/ResultsPageApp.jsx
+++ src/components/ResultsPageApp.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import SearchBox from './SearchBox.jsx';
+import NavBar from './NavBar.jsx';
+import SearchResults from './SearchResults.jsx';
 
 class ResultsPageApp extends React.Component{
   render(){
-    return <span> <SearchBox value={this.props.value} /> </span>;
+    return <span> <NavBar /> <SearchBox value={this.props.value} /> <SearchResults /></span>;
   }
 }
 
--- test/components/results_page_app_test.js
+++ test/components/results_page_app_test.js
@@ -9,7 +9,11 @@ describe('ResultsPageApp', () => {
   it('', () => {
     const resultsPageApp = mount(<ResultsPageApp value='asus tablet'/>);
 
+    expect(resultsPageApp.find("NavBar")).to.have.length(1);
+
     expect(resultsPageApp.find("SearchBox")).to.have.length(1);
     expect(resultsPageApp.find("input[type='text'][name='search']").props().value).to.eql('asus tablet');
+
+    expect(resultsPageApp.find("SearchResults")).to.have.length(1);
   });
 });
