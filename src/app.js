01898542ebb254472e80f4da1b7608deff28f800
Aldana Quintana <aldanaqm@gmail.com>
2017-02-19 12:30:07 -0300

    Add layouts

--- src/app.js
+++ src/app.js
@@ -18,7 +18,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
 Router.configure(app);
+EnvConfiguration.load();
 
 app.set('port', 3000);
 app.set('host', 'localhost');
@@ -37,10 +39,6 @@ app.use(cookieParser());
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 // app.use(express.static(path.join(__dirname, '../../public')));
 
-
-Router.configure(app);
-EnvConfiguration.load();
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -51,12 +49,12 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  let message = err.message;
+  let error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {message: message, error: error });
 });
 
 module.exports = app;
