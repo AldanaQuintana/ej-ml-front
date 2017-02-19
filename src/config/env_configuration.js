d104d6adebd32ecc67a135a8f0fe532c946a79cd
Aldana Quintana <aldanaqm@gmail.com>
2017-02-19 11:35:12 -0300

    Remove console.log

--- src/config/env_configuration.js
+++ src/config/env_configuration.js
@@ -2,13 +2,7 @@ const dotenv = require('dotenv');
 
 class EnvCOnfiguration {
   static load(){
-    console.log("--------BEFORE-------");
-    console.log(process.env.ASSET_HOST);
-    console.log(process.env.NODE_ENV);
     dotenv.config({path: `.env.${process.env.NODE_ENV}`});
-    console.log("--------AFTER-------");
-    console.log(process.env.ASSET_HOST);
-    console.log(process.env.NODE_ENV);
   }
 }
 
